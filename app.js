b3864f4b17aad9f7b9f21e7262f2db00ef9eac1d
F1renDev <F1renishere@gmail.com>
2020-01-18 16:13:57 +0300

    Working on routes

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ const placesRoutes = require("./routes/places-routes");
 
 const app = express();
 
-app.use(placesRoutes);
+app.use("/api/places", placesRoutes);
+
+// if used with 4 arguments and if any previous middleware yields an error
+// this function will be treated as a special error handler
+app.use((error, req, res, next) => {
+
+  //not sending a response if it was somehow already sent before
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500)
+  res.json({
+    message: error.message || "An unknown error occured"
+  })
+});
 
 app.listen(5000);
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,11 +1,51 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("GET request in places");
-  res.json({
-    message: "It works!"
+const TEST_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous skyscrapers in the world",
+    location: {
+      lat: 40.7483405,
+      lng: -73.9858531
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1"
+  }
+];
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+  const place = TEST_PLACES.find(p => {
+    return p.creator === userId;
   });
+  if (!place) {
+    res.status(404).json({
+      message: "Could not find a place for this user id"
+    });
+  } else {
+    res.json({
+      place: place
+    });
+  }
+});
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = TEST_PLACES.find(p => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    res.status(404).json({
+      message: "Could not find a place for this id"
+    });
+  } else {
+    res.json({
+      place: place
+    });
+  }
 });
 
 module.exports = router;
